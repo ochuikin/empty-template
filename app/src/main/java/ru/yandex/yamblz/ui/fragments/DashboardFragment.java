5d43d52c9b261033bd2af73374a37d4aa9bf5f7f
ochuykin <aq.prots@gmail.com>
2016-07-23 15:53:44 +0300

    word

--- app/src/main/java/ru/yandex/yamblz/ui/fragments/DashboardFragment.java
+++ app/src/main/java/ru/yandex/yamblz/ui/fragments/DashboardFragment.java
@@ -10,7 +10,12 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import ru.yandex.yamblz.R;
+import ru.yandex.yamblz.rules.Language;
+import ru.yandex.yamblz.rules.Word;
 
 /**
  * Created by olegchuikin on 23/07/16.
@@ -35,7 +40,16 @@ public class DashboardFragment extends BaseFragment {
         findPairTrening.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                List<Word> words = new ArrayList<>();
+                words.add(new Word(1, "1", "qwe1", Language.RU, 0.4));
+                words.add(new Word(2, "2", "qwe2", Language.RU, 0.4));
+                words.add(new Word(3, "3", "qwe3", Language.RU, 0.4));
+                words.add(new Word(4, "4", "qwe4", Language.RU, 0.4));
+                words.add(new Word(5, "5", "qwe5", Language.RU, 0.4));
+                words.add(new Word(6, "6", "qwe6", Language.RU, 0.4));
+                words.add(new Word(7, "7", "qwe7", Language.RU, 0.4));
 
+                applyFragment(FindPairFragment.create(words));
             }
         });
     }

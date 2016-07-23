261613a0c6f3429a5da87845f6efac02e75ebdc2
ochuykin <aq.prots@gmail.com>
2016-07-23 13:58:45 +0300

    settings button

--- app/src/main/java/ru/yandex/yamblz/ui/activities/MainActivity.java
+++ app/src/main/java/ru/yandex/yamblz/ui/activities/MainActivity.java
@@ -4,6 +4,9 @@ import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.SubMenu;
 
 import javax.inject.Inject;
 import javax.inject.Named;
@@ -35,7 +38,38 @@ public class MainActivity extends BaseActivity {
                     .commit();
         }
 
-        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(myToolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.main_menu, menu);
+
+//        menu.addSubMenu(0, 0, 0, "sadfasdf");
+
+        menu.addSubMenu(0, Menu.NONE, 1, "settings");
+        menu.addSubMenu(0, Menu.NONE, 2, "statistic");
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+//            case R.id.action_settings:
+                // User chose the "Settings" item, show the app settings UI...
+//                return true;
+
+            case R.id.action_setting:
+                // User chose the "Favorite" action, mark the current item
+                // as a favorite...
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
     }
 }

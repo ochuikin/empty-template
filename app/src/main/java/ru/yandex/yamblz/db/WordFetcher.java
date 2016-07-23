48b76a851412091543ffc085415c91bc04f27c43
grin3s <alexander.gringauz@gmail.com>
2016-07-23 15:12:04 +0300

    json parsed

--- app/src/main/java/ru/yandex/yamblz/db/WordFetcher.java
+++ app/src/main/java/ru/yandex/yamblz/db/WordFetcher.java
@@ -1,10 +1,12 @@
 package ru.yandex.yamblz.db;
 
 import android.content.Context;
+import android.util.JsonReader;
 import android.util.Log;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 
 /**
  * Created by grin3s on 23.07.16.
@@ -13,12 +15,23 @@ public class WordFetcher {
     public static void loadDataToDatabase(Context context) {
         try {
             InputStream istream = context.getApplicationContext().getAssets().open("words.json");
-            int size = istream.available();
-            byte[] buffer = new byte[size];
-            istream.read(buffer);
-            istream.close();
-            String bufferString = new String(buffer);
-            Log.i("TEST", bufferString);
+            JsonReader reader = new JsonReader(new InputStreamReader(istream));
+            reader.beginObject();
+            Log.i("TEST", reader.nextName());//en
+            reader.beginArray();
+            while (reader.hasNext()) {
+                String en_word = reader.nextString();
+                Log.i("TESTWORD", en_word);
+            }
+            reader.endArray();
+            Log.i("TEST", reader.nextName());//ru
+            reader.beginArray();
+            while (reader.hasNext()) {
+                String en_word = reader.nextString();
+                Log.i("TESTWORD", en_word);
+            }
+            reader.endArray();
+            reader.endObject();
         } catch (IOException e) {
             e.printStackTrace();
         }
